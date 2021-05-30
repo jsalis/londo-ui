d42259e22e79081d22ba7292fdcfb9f2942e4426
John Salis <jsalis531@gmail.com>
2021-05-30 13:50:22 -0400

    docs: add text and heading stories

---
+++ src/stories/heading.stories.jsx
@@ -0,0 +1,83 @@
+import { Heading } from "../components";
+
+export default {
+    title: "Design System/Heading",
+    component: Heading,
+    parameters: {
+        componentSubtitle: "A responsive heading component.",
+    },
+};
+
+export function Levels() {
+    return (
+        <>
+            <Heading level={1}>h1. Hello World</Heading>
+            <Heading level={2}>h2. Hello World</Heading>
+            <Heading level={3}>h3. Hello World</Heading>
+            <Heading level={4}>h4. Hello World</Heading>
+            <Heading level={5}>h5. Hello World</Heading>
+        </>
+    );
+}
+
+export function Colors() {
+    return (
+        <>
+            <Heading level={2} color="primary.base">
+                Hello World
+            </Heading>
+            <Heading level={2} color="positive.base">
+                Hello World
+            </Heading>
+            <Heading level={2} color="negative.base">
+                Hello World
+            </Heading>
+            <Heading level={2} color="warning.base">
+                Hello World
+            </Heading>
+            <Heading level={2} color="gray.base">
+                Hello World
+            </Heading>
+        </>
+    );
+}
+
+export function Disabled() {
+    return (
+        <Heading level={2} disabled>
+            Hello World
+        </Heading>
+    );
+}
+
+export function Underline() {
+    return (
+        <Heading level={2} underline>
+            Hello World
+        </Heading>
+    );
+}
+
+export function Italic() {
+    return (
+        <Heading level={2} italic>
+            Hello World
+        </Heading>
+    );
+}
+
+export function Delete() {
+    return (
+        <Heading level={2} delete>
+            Hello World
+        </Heading>
+    );
+}
+
+export function Uppercase() {
+    return (
+        <Heading level={2} textTransform="uppercase">
+            Hello World
+        </Heading>
+    );
+}
---
+++ src/stories/text.stories.jsx
@@ -0,0 +1,95 @@
+import { Text, Flex, Spacer } from "../components";
+
+export default {
+    title: "Design System/Text",
+    component: Text,
+    parameters: {
+        componentSubtitle: "A responsive inline text component.",
+    },
+};
+
+export function Basic() {
+    return <Text>Hello World</Text>;
+}
+
+export function Colors() {
+    return (
+        <Flex flexDirection="column">
+            <Spacer mb={2}>
+                <Text color="primary.base">Hello World</Text>
+                <Text color="secondary.base">Hello World</Text>
+                <Text color="positive.base">Hello World</Text>
+                <Text color="negative.base">Hello World</Text>
+                <Text color="warning.base">Hello World</Text>
+                <Text color="gray.base">Hello World</Text>
+            </Spacer>
+        </Flex>
+    );
+}
+
+export function Sizes() {
+    return (
+        <Flex flexDirection="column">
+            <Spacer mb={2}>
+                <Text fontSize={0}>Hello World</Text>
+                <Text fontSize={1}>Hello World</Text>
+                <Text fontSize={2}>Hello World</Text>
+                <Text fontSize={3}>Hello World</Text>
+                <Text fontSize={4}>Hello World</Text>
+                <Text fontSize={5}>Hello World</Text>
+                <Text fontSize={6}>Hello World</Text>
+            </Spacer>
+        </Flex>
+    );
+}
+
+export function Ellipsis() {
+    return (
+        <Text width={1 / 2} ellipsis>
+            Nunc sed blandit libero volutpat sed cras ornare arcu. Facilisi morbi tempus
+            iaculis urna id volutpat lacus laoreet non. Varius duis at consectetur lorem
+            donec massa sapien faucibus. Id aliquet lectus proin nibh. Quis varius quam
+            quisque id diam vel quam elementum. Id consectetur purus ut faucibus pulvinar.
+        </Text>
+    );
+}
+
+export function Disabled() {
+    return <Text disabled>Hello World</Text>;
+}
+
+export function Strong() {
+    return <Text strong>Hello World</Text>;
+}
+
+export function Underline() {
+    return <Text underline>Hello World</Text>;
+}
+
+export function Italic() {
+    return <Text italic>Hello World</Text>;
+}
+
+export function Delete() {
+    return <Text delete>Hello World</Text>;
+}
+
+export function Code() {
+    return <Text code>Hello World</Text>;
+}
+
+export function Keyboard() {
+    return (
+        <>
+            <Text keyboard>Ctrl</Text>
+            {" + "}
+            <Text keyboard>Shift</Text>
+            {" + "}
+            <Text keyboard>R</Text>
+        </>
+    );
+}
+
+export function Uppercase() {
+    return <Text textTransform="uppercase">Hello World</Text>;
+}
