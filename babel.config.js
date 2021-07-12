77a34f06c5fa07aa3a8d7c55093770dd0f20f6fc
John Salis <jsalis531@gmail.com>
2021-07-11 22:26:06 -0400

    build: add build and release scripts

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
 module.exports = (api) => {
-    const envConfig = api.env("test")
-        ? { targets: { node: "current" } }
-        : { modules: false };
+    const envConfig = api.env("test") ? { targets: { node: "current" } } : { modules: false };
     return {
         presets: [["@babel/preset-env", envConfig], "@babel/preset-react"],
         plugins: ["babel-plugin-styled-components"],
+        ignore: ["**/*.stories.jsx"],
     };
 };
