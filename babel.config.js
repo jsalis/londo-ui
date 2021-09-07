7c36e38b398b6099482adcfa1e6e437835f2c4c7
John Salis <jsalis531@gmail.com>
2021-09-07 00:20:37 -0400

    build: configure runtime for react 17

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = (api) => {
     const envConfig = api.env("test") ? { targets: { node: "current" } } : { modules: false };
     return {
-        presets: [["@babel/preset-env", envConfig], "@babel/preset-react"],
+        presets: [
+            ["@babel/preset-env", envConfig],
+            ["@babel/preset-react", { runtime: "automatic" }],
+        ],
         plugins: ["babel-plugin-styled-components"],
     };
 };
