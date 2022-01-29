ccf6e060950e4168c510fd1676a2acffad9a7243
John Salis <jsalis531@gmail.com>
2022-01-29 16:54:49 -0500

    build: set up typescript

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = (api) => {
     return {
         presets: [
             ["@babel/preset-env", envConfig],
+            ["@babel/preset-typescript"],
             ["@babel/preset-react", { runtime: "automatic" }],
         ],
         plugins: ["babel-plugin-styled-components"],
