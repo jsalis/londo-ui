21f384f0238f395f4777c896e207a46c1958fb7c
John Salis <jsalis531@gmail.com>
2023-08-07 02:35:35 -0400

    fix: add epsilon to round function

--- src/utils/math-util.ts
+++ src/utils/math-util.ts
@@ -1,13 +1,13 @@
 /**
  * Clamps a value between a minimum and maximum.
  */
-export function clamp(value: number, min = 0, max = 1) {
-    return value < min ? min : value > max ? max : value;
+export function clamp(val: number, min = 0, max = 1) {
+    return val < min ? min : val > max ? max : val;
 }
 
 /**
  * Rounds a value to a given number of digits.
  */
-export function round(number: number, digits = 0, base = 10 ** digits) {
-    return Math.round(number * base) / base;
+export function round(val: number, digits = 0, base = 10 ** digits) {
+    return Math.round((val + Number.EPSILON) * base) / base;
 }
