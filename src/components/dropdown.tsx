ffb4a79d344dd75ad7b1f3cd5ef21c43912268d0
John Salis <jsalis531@gmail.com>
2024-01-13 10:33:45 -0500

    feat: remove floater from dropdown component and add props for strategy and container

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,22 +1,34 @@
-import { Children, isValidElement, cloneElement, forwardRef, useRef } from "react";
+import type { Placement, Strategy } from "@floating-ui/react-dom";
+import {
+    Children,
+    isValidElement,
+    cloneElement,
+    forwardRef,
+    useState,
+    useEffect,
+    useLayoutEffect,
+} from "react";
+import { useFloating, autoUpdate, offset, flip, shift, size } from "@floating-ui/react-dom";
 
 import { useForkRef, useForkHandler } from "../hooks";
 
 import type { BoxProps } from "./box";
-import type { FloaterProps } from "./floater";
 import { Box } from "./box";
-import { Floater } from "./floater";
+import { Portal } from "./portal";
 import { ClickAwayListener } from "./click-away-listener";
+import { getTransformOrigin } from "../utils/floating-util";
 
 export interface DropdownProps extends BoxProps {
     overlay?: React.ReactNode;
-    placement?: FloaterProps["placement"];
+    placement?: Placement;
+    strategy?: Strategy;
     isOpen: boolean;
     onOpen?: () => void;
     onClose?: () => void;
     disabled?: boolean;
     keepMounted?: boolean;
     matchWidth?: boolean;
+    container?: HTMLElement | (() => HTMLElement);
     children?: React.ReactNode;
 }
 
@@ -25,6 +37,7 @@ export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
         {
             overlay,
             placement = "bottom-start",
+            strategy = "absolute",
             isOpen,
             onOpen,
             onClose,
@@ -32,6 +45,7 @@ export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
             keepMounted = true,
             matchWidth = true,
             color,
+            container,
             children,
             ...rest
         },
@@ -53,40 +67,87 @@ export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
             }
         };
 
+        const [anchorNode, setAnchorNode] = useState<HTMLElement | null>(null);
         const child = Children.only(children);
-        const anchorRef = useRef<HTMLElement>();
         const childProps = {
-            ref: useForkRef((child as any).ref, anchorRef),
+            ref: useForkRef((child as any).ref, setAnchorNode),
             onClick: useForkHandler((child as any).props.onClick, handleClick),
         };
 
         const anchor = isValidElement(child) ? cloneElement(child, childProps) : child;
 
+        const middleware = [
+            offset(4),
+            flip({ padding: 8 }),
+            shift({ padding: 8 }),
+            size({
+                apply({ rects, elements }) {
+                    if (matchWidth) {
+                        Object.assign(elements.floating.style, {
+                            width: `${rects.reference.width}px`,
+                        });
+                    }
+                },
+            }),
+        ];
+
+        const {
+            x,
+            y,
+            refs,
+            reference,
+            floating,
+            update,
+            placement: autoPlacement,
+        } = useFloating({
+            strategy,
+            placement,
+            middleware,
+        });
+
+        useLayoutEffect(() => reference(anchorNode), [anchorNode]);
+        useLayoutEffect(() => update(), [isOpen, refs.floating.current]);
+
+        useEffect(() => {
+            if (isOpen && refs.reference.current && refs.floating.current) {
+                return autoUpdate(refs.reference.current, refs.floating.current, update);
+            }
+            return;
+        }, [isOpen, refs.reference.current, refs.floating.current, update]);
+
         return (
             <ClickAwayListener onClickAway={handleClickAway}>
                 <div>
                     {anchor}
-                    <Floater
-                        ref={ref}
-                        offset={4}
-                        anchor={anchorRef.current}
-                        isOpen={isOpen}
-                        placement={placement}
-                        keepMounted={keepMounted}
-                        matchWidth={matchWidth}
-                        onMouseDown={(e: React.MouseEvent) => e.preventDefault()}
-                    >
-                        <Box
-                            py={1}
-                            borderRadius="base"
-                            boxShadow="base"
-                            bg="dropdown.bg"
-                            color={color as any}
-                            {...rest}
-                        >
-                            {overlay}
-                        </Box>
-                    </Floater>
+                    {isOpen || keepMounted ? (
+                        <Portal container={container}>
+                            <div
+                                ref={floating}
+                                role="tooltip"
+                                style={{
+                                    position: strategy,
+                                    top: y ?? "",
+                                    left: x ?? "",
+                                    transformOrigin: getTransformOrigin(autoPlacement),
+                                    display: !isOpen && keepMounted ? "none" : "",
+                                    zIndex: 1,
+                                }}
+                                onMouseDown={(e: React.MouseEvent) => e.preventDefault()}
+                            >
+                                <Box
+                                    ref={ref}
+                                    py={1}
+                                    borderRadius="base"
+                                    boxShadow="base"
+                                    bg="dropdown.bg"
+                                    color={color as any}
+                                    {...rest}
+                                >
+                                    {overlay}
+                                </Box>
+                            </div>
+                        </Portal>
+                    ) : null}
                 </div>
             </ClickAwayListener>
         );
