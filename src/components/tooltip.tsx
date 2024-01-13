e64c6884c4a61e207691cc06ce481a1019b343e8
John Salis <jsalis531@gmail.com>
2024-01-13 08:05:09 -0500

    feat: remove floater from tooltip component and add props for strategy and container

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,19 +1,31 @@
-import { Children, isValidElement, cloneElement, forwardRef, useState } from "react";
+import type { Placement, Strategy } from "@floating-ui/react-dom";
+import {
+    Children,
+    isValidElement,
+    cloneElement,
+    forwardRef,
+    useState,
+    useEffect,
+    useLayoutEffect,
+} from "react";
+import { useFloating, autoUpdate, offset, flip, shift, size } from "@floating-ui/react-dom";
 
 import { useDisclosure, useForkRef, useEventListener } from "../hooks";
 
 import type { BoxProps } from "./box";
-import type { FloaterProps } from "./floater";
 import { Box } from "./box";
-import { Floater } from "./floater";
+import { Portal } from "./portal";
+import { getTransformOrigin } from "../utils/floating-util";
 
 export interface TooltipProps extends BoxProps {
     title?: React.ReactNode;
-    placement?: FloaterProps["placement"];
+    placement?: Placement;
+    strategy?: Strategy;
     mouseEnterDelay?: number;
     mouseLeaveDelay?: number;
     onOpen?: () => void;
     onClose?: () => void;
+    container?: HTMLElement | (() => HTMLElement);
     children?: React.ReactNode;
 }
 
@@ -22,10 +34,12 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
         {
             title,
             placement = "top",
+            strategy = "absolute",
             mouseEnterDelay = 100,
             mouseLeaveDelay = 100,
             onOpen,
             onClose,
+            container,
             children,
             ...rest
         },
@@ -41,6 +55,32 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
         const childProps = { ref: useForkRef((child as any).ref, setAnchorNode) };
         const anchor = isValidElement(child) ? cloneElement(child, childProps) : child;
 
+        const middleware = [offset(8), flip({ padding: 8 }), shift({ padding: 8 }), size()];
+
+        const {
+            x,
+            y,
+            refs,
+            reference,
+            floating,
+            update,
+            placement: autoPlacement,
+        } = useFloating({
+            strategy,
+            placement,
+            middleware,
+        });
+
+        useLayoutEffect(() => reference(anchorNode), [anchorNode]);
+        useLayoutEffect(() => update(), [isOpen, refs.floating.current]);
+
+        useEffect(() => {
+            if (isOpen && refs.reference.current && refs.floating.current) {
+                return autoUpdate(refs.reference.current, refs.floating.current, update);
+            }
+            return;
+        }, [isOpen, refs.reference.current, refs.floating.current, update]);
+
         useEventListener("mouseenter", openWithDelay, anchorNode);
         useEventListener("mouseleave", closeWithDelay, anchorNode);
         useEventListener("focus", openWithDelay, anchorNode);
@@ -49,26 +89,35 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
         return (
             <>
                 {anchor}
-                <Floater
-                    anchor={anchorNode}
-                    isOpen={isOpen}
-                    placement={placement}
-                    style={{ pointerEvents: "none" }}
-                >
-                    <Box
-                        ref={ref}
-                        p={1}
-                        borderRadius="base"
-                        boxShadow="base"
-                        bg="tooltip.bg"
-                        color={"tooltip.text" as any}
-                        onMouseEnter={clearDelayTimer}
-                        onMouseLeave={closeWithDelay}
-                        {...rest}
-                    >
-                        {title}
-                    </Box>
-                </Floater>
+                {isOpen ? (
+                    <Portal container={container}>
+                        <div
+                            ref={floating}
+                            role="tooltip"
+                            style={{
+                                position: strategy,
+                                top: y ?? "",
+                                left: x ?? "",
+                                transformOrigin: getTransformOrigin(autoPlacement),
+                                zIndex: 1,
+                            }}
+                        >
+                            <Box
+                                ref={ref}
+                                p={1}
+                                borderRadius="base"
+                                boxShadow="base"
+                                bg="tooltip.bg"
+                                color={"tooltip.text" as any}
+                                onMouseEnter={clearDelayTimer}
+                                onMouseLeave={closeWithDelay}
+                                {...rest}
+                            >
+                                {title}
+                            </Box>
+                        </div>
+                    </Portal>
+                ) : null}
             </>
         );
     }
