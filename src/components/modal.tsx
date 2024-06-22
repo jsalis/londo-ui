289424472d08bd70b1e64c0a80f1312dcf48a79c
John Salis <jsalis531@gmail.com>
2024-06-22 17:35:18 -0400

    fix: modal transition is too slow

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -145,7 +145,7 @@ const Content = forwardRef<HTMLDivElement, ModalContentProps>((props, ref) => {
                         initial={{ opacity: 0, scale: 0.8 }}
                         animate={{ opacity: 1, scale: 1 }}
                         exit={{ opacity: 0, scale: 0.8 }}
-                        transition={{ type: "spring", damping: 20, stiffness: 300 }}
+                        transition={{ type: "easeInOut", duration: 0.2 }}
                         {...sectionProps}
                     >
                         {children}
